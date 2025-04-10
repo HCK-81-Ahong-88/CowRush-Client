459ae8621005116c31c2c7643b6240b3cdb490dc
Wanda Budiono <wandaabudiono@gmail.com>
2025-04-10 11:00:03 +0700

    Fix : For Deployment

--- src/contexts/socket.context.jsx
+++ src/contexts/socket.context.jsx
@@ -2,7 +2,7 @@
 import { createContext, useContext } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://ahong88.cowrush.fun");
 const SocketContext = createContext(socket);
 
 export const useSocket = () => useContext(SocketContext);
